1640a5ce92ad11cd4bd715e8c707a12fade9c9a9
trempenault <trempenault@gmail.com>
2013-03-13 10:01:15 +0100

    creation repo

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="design.css" />
+</head>
+
+<body>
+<?php include("header.php"); ?>
+<div id="center">
+ssssssss
+</div>
+</body>
+</html>
